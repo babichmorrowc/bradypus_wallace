2974afcf6e0dd2f5c93d7f4face52821caeb2f3e
Cecina Babich Morrow <babichmorrowc@kenyon.edu>
2018-10-09 14:50:32 -0400

    fix Env to Env_sloths, add niche equivalence test

--- ecospat_tutorial_bradypus.R
+++ ecospat_tutorial_bradypus.R
@@ -33,18 +33,18 @@ points(variegatus[,2:3], pch = 20, col = "darkorchid4")
 points(tridactylus[,2:3], pch = 20, col = "deeppink3")
 
 #background by radius
-bg_var = background.points.buffer(variegatus[,2:3], radius = 200000, n = 10*nrow(variegatus), mask = Env[[1]])
-bg_tri = background.points.buffer(tridactylus[,2:3], radius = 200000, n = 10*nrow(tridactylus), mask = Env[[1]])
+bg_var = background.points.buffer(variegatus[,2:3], radius = 200000, n = 10*nrow(variegatus), mask = Env_sloths[[1]])
+bg_tri = background.points.buffer(tridactylus[,2:3], radius = 200000, n = 10*nrow(tridactylus), mask = Env_sloths[[1]])
 
 # Get environmental data
-extract_var = na.omit(cbind(variegatus[,2:3], extract(Env, variegatus[,2:3]), rep(1, nrow(variegatus))))
-extract_tri = na.omit(cbind(tridactylus[,2:3], extract(Env, tridactylus[,2:3]), rep(1, nrow(tridactylus))))
+extract_var = na.omit(cbind(variegatus[,2:3], extract(Env_sloths, variegatus[,2:3]), rep(1, nrow(variegatus))))
+extract_tri = na.omit(cbind(tridactylus[,2:3], extract(Env_sloths, tridactylus[,2:3]), rep(1, nrow(tridactylus))))
 
 colnames(extract_var)[ncol(extract_var)] = 'occ'
 colnames(extract_tri)[ncol(extract_tri)] = 'occ'
 
-extbg_var = na.omit(cbind(bg_var, extract(Env, bg_var), rep(0, nrow(bg_var))))
-extbg_tri = na.omit(cbind(bg_tri, extract(Env, bg_tri), rep(0, nrow(bg_tri))))
+extbg_var = na.omit(cbind(bg_var, extract(Env_sloths, bg_var), rep(0, nrow(bg_var))))
+extbg_tri = na.omit(cbind(bg_tri, extract(Env_sloths, bg_tri), rep(0, nrow(bg_tri))))
 
 colnames(extbg_var)[ncol(extbg_var)] = 'occ'
 colnames(extbg_tri)[ncol(extbg_tri)] = 'occ'
@@ -66,10 +66,10 @@ ecospat.plot.contrib(contrib=pca.env_sloth$co, eigen=pca.env_sloth$eig)
 #Calculating various pca scores
 scores.globclim<-pca.env_sloth$li # PCA scores for the whole study area
 
-scores.var <- suprow(pca.env,
+scores.var <- suprow(pca.env_sloth,
                      extract_var[which(extract_var[,22]==1),3:21])$li # PCA scores for the species 1 distribution
 
-scores.tri <- suprow(pca.env,
+scores.tri <- suprow(pca.env_sloth,
                      extract_tri[which(extract_tri[,22]==1),3:21])$li # PCA scores for the species 1 distribution
 
 scores.clim_var <- suprow(pca.env_sloth,dat_var[,3:21])$li # PCA scores for the whole native study area
@@ -82,23 +82,27 @@ grid.clim_var <- ecospat.grid.clim.dyn(
   glob = scores.globclim,
   glob1 = scores.clim_var,
   sp = scores.var,
-  R = 100,
-  th.sp = 0
+  R = 100
 )
 
-#get the following error:
-#Error in quantile.default(spr, th.sp) : 
-#  missing values and NaN's not allowed if 'na.rm' is FALSE
 
-grid.clim2 <- ecospat.grid.clim.dyn(
+grid.clim_tri <- ecospat.grid.clim.dyn(
   glob = scores.globclim,
-  glob1 = scores.clim2,
-  sp = scores.sp2,
+  glob1 = scores.clim_tri,
+  sp = scores.tri,
   R = 100,
   th.sp = 0
 )
 
-D.overlap <- ecospat.niche.overlap (grid.clim1, grid.clim2, cor=T)$D 
+#Calculate Schoener's overlap metric
+D.overlap <- ecospat.niche.overlap (grid.clim_var, grid.clim_tri, cor=T)$D 
 D.overlap
 
-
+#Niche equivalence test
+eq.test <- ecospat.niche.equivalency.test(grid.clim_var, grid.clim_tri,
+                                          rep=10, alternative = "greater") ##rep = 1000 recommended for operational runs
+sim.test <- ecospat.niche.similarity.test(grid.clim_var, grid.clim_tri,
+                                          rep=1000, alternative = "greater",
+                                          rand.type=2) 
+ecospat.plot.overlap.test(eq.test, "D", "Equivalency")
+ecospat.plot.overlap.test(sim.test, "D", "Similarity")
