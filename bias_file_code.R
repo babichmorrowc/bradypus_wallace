3e6c3d4301d8f94def2fdb5ecdce83df2f9b147d
babichmorrowc <babichmorrowc@kenyon.edu>
2018-11-14 11:13:01 -0500

    remove rows with duplicate occurrences, NA values, and erroneous lat/long values

--- bias_file_code.R
+++ bias_file_code.R
@@ -1,6 +1,12 @@
 #Load packages
 library(spocc)
 library(plyr)
+library(viridis)
+library(ggmap)
+
+api_key = "AIzaSyBK7lLbqoqnYFdzf-idYYposb-1gwyRAlQ"
+register_google(key = api_key)
+
 
 #import csv of species list for bias file
 #includes primates, sloths, margay, olingos, mountain coatis, coatis, and kinkajous
@@ -28,6 +34,15 @@ for(i in 2:length(sp_vec)){
   }
 }
 
+#remove rows with duplicated occurrences
+bg.dups <- duplicated(bg_locations[c('longitude', 'latitude')])
+bg_locations <- bg_locations[!bg.dups,]
+#get rid of occurrences with NA values for latitude or longitude
+bg_locations <- bg_locations[which(!is.na(bg_locations$latitude) | !is.na(bg_locations$longitude)),]
+#remove locations with latitude/longitude values outside of extent
+bg_locations <- subset(bg_locations, longitude > -180 & longitude <180 & latitude > -60 & latitude < 90)
+
+
 #Plot the background points
 #Make dataframe:
 world_map <- map_data("world")
@@ -39,3 +54,29 @@ world +
   geom_point(data = bg_locations, aes(x = longitude, y = latitude),
              color = "green",
              size = 1)
+#"zoom in" to region around variegatus points
+var_bound_box <- make_bbox(lon = variegatus$longitude, lat = variegatus$latitude, f = .5)
+#Get a satellite map at the location of the bounding box:
+var_bbox_map <- get_map(location = var_bound_box, maptype = "satellite", source = "google")
+ggmap(var_bbox_map) + 
+  geom_point(data = bg_locations, aes(x = longitude, y = latitude), 
+             color = "red",
+             size =1)
+
+# Create a two column dataframe of the non-NA occurrence points
+bg_coords <- bg_locations[c('longitude', 'latitude')]
+
+#We need the occurrence raster to have the same resolution as our climate data
+#First import climate data
+grids = list.files("/Users/hellenfellows/Desktop/bio_2-5m_bil", pattern = "*.bil$")
+sta = stack(paste0("/Users/hellenfellows/Desktop/bio_2-5m_bil/", grids))
+brick <- brick(sta)
+plot(sta[[1]], col = viridis(99))
+points(bg_coords, col = "red", pch = 0.5)
+
+#Make occurrence raster for background points
+bg_raster <- rasterize(bg_coords, brick, field = 1)
+#bg_raster <- rasterize(bg_coords, Env_sloths, field = 1)
+length(na.omit(bg_raster@data@values))
+plot(bg_raster, col = "black")
+
