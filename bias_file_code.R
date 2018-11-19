419c01450b2f217f0ac4d4a80637b1f35ec543f9
babichmorrowc <babichmorrowc@kenyon.edu>
2018-11-19 13:35:33 -0500

    add code to interrogate bias layer

--- bias_file_code.R
+++ bias_file_code.R
@@ -5,6 +5,7 @@ library(viridis)
 library(ggmap)
 library(MASS)
 library(ENMeval)
+library(rgbif)
 
 #provide API key
 #api_key = 
@@ -38,6 +39,7 @@ for(i in 2:length(sp_vec)){
   }
 }
 
+
 #remove rows with duplicated occurrences
 bg.dups <- duplicated(bg_locations[c('longitude', 'latitude')])
 bg_locations <- bg_locations[!bg.dups,]
@@ -60,6 +62,7 @@ world +
 var_bound_box <- make_bbox(lon = variegatus$longitude, lat = variegatus$latitude, f = .5)
 #Get a satellite map at the location of the bounding box:
 var_bbox_map <- get_map(location = var_bound_box, maptype = "satellite", source = "google")
+var_bbox_map2 <- get_googlemap(location = var_bound_box, style = 'feature:administrative.country|element:labels|visibility:off')
 ggmap(var_bbox_map) + 
   geom_point(data = bg_locations, aes(x = longitude, y = latitude), 
              color = "red",
@@ -106,8 +109,109 @@ plot(dens_ras)
 
 # mask bias file
 #crop bias file to extent of sloth data
-bias <- raster::mask(bias, Env_sloths[[1]])
+bias <- raster::mask(dens_ras, Env_sloths[[1]])
 plot(bias)
 
 #Save bias layer
-writeRaster(dens_ras, "sloth_bias_file.tif")
+writeRaster(bias, "sloth_bias_file.tif", overwrite = TRUE)
+
+########################################################################################################
+
+#"interrogating" the bias layer
+
+#Use rgbif::occ_data() to get the locality information with data source
+rgbif_occ_1 <- occ_data(scientificName = sp_vec[1], limit = 1500)
+rgbif_df_1 <- rgbif_occ_1$data
+rgbif_bg_data <- rgbif_df_1
+
+for(i in 2:length(sp_vec)){
+  rgbif_occ_i <- occ_data(scientificName = sp_vec[i], limit = 1000)
+  rgbif_df_i <- rgbif_occ_i$data
+  if(length(rgbif_df_i) == 0){
+    print(paste(i, ": No observations in GBIF", sep = ""))
+  } else{
+    rgbif_bg_data <- rbind.fill(rgbif_bg_data, rgbif_df_i)
+  }
+}
+
+rgbif_bg_data <- as_data_frame(rgbif_bg_data)
+
+#see datasets and record bases
+unique(rgbif_bg_data$datasetName)
+
+unique(rgbif_bg_data$basisOfRecord)
+ggplot(data = rgbif_bg_data, aes(x = basisOfRecord)) + 
+  geom_bar(stat = "count") +
+  theme(axis.text.x = element_text(angle = 90))
+ggmap(var_bbox_map) + 
+  geom_point(data = rgbif_bg_data, aes(x = decimalLongitude, y = decimalLatitude, color = basisOfRecord)) +
+  scale_color_viridis(option = "B", discrete = T)
+
+
+#remove rows with duplicated occurrences
+rgbif.dups <- duplicated(rgbif_bg_data[c('decimalLongitude', 'decimalLatitude')])
+ggplot(data = rgbif_bg_data[rgbif.dups,], aes(x = basisOfRecord)) + 
+  geom_bar(stat = "count") +
+  theme(axis.text.x = element_text(angle = 90))
+rgbif_nodups_data <- rgbif_bg_data[!rgbif.dups,]
+#get rid of occurrences with NA values for latitude or longitude
+rgbif_nodups_data <- rgbif_nodups_data[which(!is.na(rgbif_nodups_data$decimalLatitude) | !is.na(rgbif_nodups_data$decimalLongitude)),]
+#remove locations with latitude/longitude values outside of extent
+rgbif_nodups_data <- subset(rgbif_nodups_data, decimalLongitude > -180 & decimalLongitude <180 & decimalLatitude > -90 & decimalLatitude < 90)
+
+unique(rgbif_nodups_data$basisOfRecord)
+ggplot(data = rgbif_nodups_data, aes(x = basisOfRecord)) + 
+  geom_bar(stat = "count") +
+  theme(axis.text.x = element_text(angle = 90))
+ggmap(var_bbox_map) + 
+  geom_point(data = rgbif_nodups_data, aes(x = decimalLongitude, y = decimalLatitude, color = basisOfRecord)) +
+  scale_color_viridis(option = "B", discrete = T)
+
+ggmap(var_bbox_map) + 
+  geom_point(data = rgbif_nodups_data[rgbif_nodups_data$basisOfRecord == "HUMAN_OBSERVATION",], aes(x = decimalLongitude, y = decimalLatitude, color = basisOfRecord)) +
+  scale_color_viridis(option = "B", discrete = T)
+ggmap(var_bbox_map) + 
+  geom_point(data = rgbif_nodups_data[rgbif_nodups_data$basisOfRecord == "PRESERVED_SPECIMEN",], aes(x = decimalLongitude, y = decimalLatitude, color = basisOfRecord)) +
+  scale_color_viridis(option = "B", discrete = T)
+
+
+unique(rgbif_nodups_data$datasetName)
+length(which(rgbif_nodups_data$datasetName == "iNaturalist research-grade observations"))
+ggmap(var_bbox_map) + 
+  geom_point(data = rgbif_nodups_data, aes(x = decimalLongitude, y = decimalLatitude), color = "black") +
+  geom_point(data = rgbif_nodups_data[rgbif_nodups_data$datasetName == "iNaturalist research-grade observations",], aes(x = decimalLongitude, y = decimalLatitude), color = "red", alpha = 0.25)
+
+#remove iNaturalist observations
+rgbif_noiNat <- rgbif_nodups_data[which(rgbif_nodups_data$datasetName != "iNaturalist research-grade observations"),]
+ggmap(var_bbox_map) +
+  geom_point(data = rgbif_noiNat, aes(x = decimalLongitude, y = decimalLatitude), color = "red")
+
+
+# Create a two column dataframe of the non-NA occurrence points
+noiNat_coords <- rgbif_noiNat[c('decimalLongitude', 'decimalLatitude')]
+#Create SpatialPoints object of occurrence points
+noiNat_spatialpoints <- SpatialPoints(rgbif_noiNat[c('decimalLongitude', 'decimalLatitude')])
+
+#Make occurrence raster for points without iNaturalist
+noiNat_raster <- rasterize(noiNat_spatialpoints, brick, field = 1)
+length(na.omit(noiNat_raster@data@values))
+
+#occurrence raster cropped to sloth extent
+noiNat_raster_crop <- rasterize(noiNat_coords, brick_sloths, field = 1)
+length(na.omit(noiNat_raster_crop@data@values))
+plot(noiNat_raster_crop, col = "black")
+
+#Making KDE
+noiNat_presence <- which(values(noiNat_raster_crop) == 1)
+noiNat_presence_locs <- coordinates(noiNat_raster_crop)[noiNat_presence,]
+noiNat_dens <- kde2d(noiNat_presence_locs[,1], noiNat_presence_locs[,2], n = c(nrow(noiNat_raster_crop), ncol(noiNat_raster_crop)))
+noiNat_dens_ras <- raster(noiNat_dens)
+#resample to make dens_ras the same resolution of brick_sloths
+noiNat_dens_ras <- resample(noiNat_dens_ras, brick_sloths, method = "bilinear")
+plot(noiNat_dens_ras)
+
+# mask bias file
+#crop bias file to extent of sloth data
+noiNat_bias <- raster::mask(noiNat_dens_ras, Env_sloths[[1]])
+plot(noiNat_bias)
+points(noiNat_coords)
