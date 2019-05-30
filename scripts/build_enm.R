fe63ad39805c6fe61e626a4770802ac431bedd16
babichmorrowc <babichmorrowc@kenyon.edu>
2019-05-30 13:41:02 -0400

    checkerboard agg 10; ENMeval dev branch edits

--- scripts/build_enm.R
+++ scripts/build_enm.R
@@ -1,16 +1,16 @@
-build_enm <- function(occs, env, bg.coords, partition, aggregation.factor){
+build_enm <- function(occs, env, bg, partition, aggregation.factor){
   # partition the occurrence data
   if(partition == "block"){
-    group.data <- ENMeval::get.block(occ = occs, bg.coords = bg.coords)
+    group.data <- ENMeval::get.block(occ = occs, bg = bg)
   } else if(partition == "checkerboard1"){
-    group.data <- ENMeval::get.checkerboard1(occ = occs, env = env, bg.coords = bg.coords, aggregation.factor = aggregation.factor)
+    group.data <- ENMeval::get.checkerboard1(occ = occs, env = env, bg = bg, aggregation.factor = aggregation.factor)
   } else if(partition == "checkerboard2"){
-    group.data <- ENMeval::get.checkerboard2(occ = occs, env = env, bg.coords = bg.coords, aggregation.factor = aggregation.factor)
+    group.data <- ENMeval::get.checkerboard2(occ = occs, env = env, bg = bg, aggregation.factor = aggregation.factor)
   }
   occs.grp <- group.data[[1]]
   bg.grp <- group.data[[2]]
   rms <- seq(0.5, 5, 0.5)
-  enm <- ENMeval::ENMevaluate(occs, env, bg.coords = bg.coords, RMvalues = rms, fc = c('L', 'LQ', 'LH', 'LQH'), 
+  enm <- ENMeval::ENMevaluate(occs, env, bg = bg, RMvalues = rms, fc = c('L', 'LQ', 'LH', 'LQH'), 
                                         occ.grp = occs.grp, bg.grp = bg.grp, method = 'user', clamp = TRUE, algorithm = "maxnet")
   return(enm)
 }
--- scripts/partition_options.R
+++ scripts/partition_options.R
@@ -3,6 +3,21 @@
 
 # VARIEGATUS --------------------------------------------------------------
 
+# Block -------------------------------------------------------------------
+
+var_block <- build_enm(occs = thinned_var_occs[,2:3], env = var_envsBgMsk,
+                       bg.coords = buffer_var_bg.xy, partition = "block")
+var_block_eval <- var_block@results
+var_block_eval <- var_block_eval[with(var_block_eval, order(avg.test.or10pct, -avg.test.AUC, delta.AICc)), ]
+View(var_block_eval)
+# get model
+var_block_evalMods <- var_block@models
+names(var_block_evalMods) <- var_block@results$settings
+var_block_Preds <- var_block@predictions
+var_block_mod <- var_block_evalMods[["LH_1"]]
+var_block_proj <- ENMeval::maxnet.predictRaster(var_block_mod, Env_var, type = 'cloglog', clamp = TRUE)
+#plot the model prediction
+plot(var_block_proj)
 
 # Checkerboard 1 ----------------------------------------------------------
 
@@ -38,6 +53,19 @@ var_checkerboard1.4 <- build_enm(occs = thinned_var_occs[,2:3], env = var_envsBg
 var_checkerboard1.4_eval <- var_checkerboard1.4@results
 var_checkerboard1.4_eval <- var_checkerboard1.4_eval[with(var_checkerboard1.4_eval, order(avg.test.or10pct, -avg.test.AUC, delta.AICc)), ]
 View(var_checkerboard1.4_eval)
+
+var_checkerboard1.10 <- build_enm(occs = thinned_var_occs[,2:3], env = var_envsBgMsk,
+                         bg = buffer_var_bg.xy, partition = "checkerboard1", aggregation.factor = 10)
+var_checkerboard1.10_eval <- var_checkerboard1.10@results
+var_checkerboard1.10_eval <- var_checkerboard1.10_eval[with(var_checkerboard1.10_eval, order(avg.test.or10pct, -avg.test.AUC, delta.AICc)), ]
+View(var_checkerboard1.10_eval)
+# get model
+var_checkerboard1.10_evalMods <- var_checkerboard1.10@models
+var_checkerboard1.10_Preds <- var_checkerboard1.10@predictions
+var_checkerboard1.10_mod <- var_checkerboard1.10_evalMods[["L2.0"]]
+var_checkerboard1.10_proj <- ENMeval::maxnet.predictRaster(var_checkerboard1.10_mod, Env_var, type = 'cloglog', doClamp = TRUE)
+#plot the model prediction
+plot(var_checkerboard1.10_proj)
 
 # Checkerboard 2 ----------------------------------------------------------
 
@@ -128,6 +156,19 @@ tri_checkerboard1.2_proj <- ENMeval::maxnet.predictRaster(tri_checkerboard1.2_mo
 #plot the model prediction
 plot(tri_checkerboard1.2_proj)
 
+tri_checkerboard1.10 <- build_enm(occs = thinned_tri_occs[,2:3], env = tri_envsBgMsk,
+                                  bg = buffer_tri_bg.xy, partition = "checkerboard1", aggregation.factor = 10)
+tri_checkerboard1.10_eval <- tri_checkerboard1.10@results
+tri_checkerboard1.10_eval <- tri_checkerboard1.10_eval[with(tri_checkerboard1.10_eval, order(avg.test.or10pct, -avg.test.AUC, delta.AICc)), ]
+View(tri_checkerboard1.10_eval)
+# get model
+tri_checkerboard1.10_evalMods <- tri_checkerboard1.10@models
+tri_checkerboard1.10_Preds <- tri_checkerboard1.10@predictions
+tri_checkerboard1.10_mod <- tri_checkerboard1.10_evalMods[["LH3.0"]]
+tri_checkerboard1.10_proj <- ENMeval::maxnet.predictRaster(tri_checkerboard1.10_mod, Env_tri, type = 'cloglog', doClamp = TRUE)
+#plot the model prediction
+plot(tri_checkerboard1.10_proj)
+
 # Checkerboard 2 ----------------------------------------------------------
 
 
@@ -148,6 +189,7 @@ tri_checkerboard2.1_mod <- tri_checkerboard2.1_evalMods[["LQ_5"]]
 tri_checkerboard2.1_proj <- ENMeval::maxnet.predictRaster(tri_checkerboard2.1_mod, Env_tri, type = 'cloglog', clamp = TRUE)
 #plot the model prediction
 plot(tri_checkerboard2.1_proj)
+points(tridactylus_occs[,2:3])
 
 tri_checkerboard2.2_eval <- tri_checkerboard2.2@results
 tri_checkerboard2.2_eval <- tri_checkerboard2.2_eval[with(tri_checkerboard2.2_eval, order(avg.test.or10pct, -avg.test.AUC, delta.AICc)), ]
@@ -156,9 +198,105 @@ View(tri_checkerboard2.2_eval)
 tri_checkerboard2.2_evalMods <- tri_checkerboard2.2@models
 names(tri_checkerboard2.2_evalMods) <- tri_checkerboard2.2@results$settings
 tri_checkerboard2.2_Preds <- tri_checkerboard2.2@predictions
-tri_checkerboard2.2_mod <- tri_checkerboard2.2_evalMods[["L_4.5"]]
+tri_checkerboard2.2_mod <- tri_checkerboard2.2_evalMods[["L_0.5"]]
 tri_checkerboard2.2_proj <- ENMeval::maxnet.predictRaster(tri_checkerboard2.2_mod, Env_tri, type = 'cloglog', clamp = TRUE)
 #plot the model prediction
 plot(tri_checkerboard2.2_proj)
 
-  
+# TORQUATUS ---------------------------------------------------------------
+
+
+# Block -------------------------------------------------------------------
+
+tor_block <- build_enm(occs = thinned_tor_occs[,2:3], env = tor_envsBgMsk,
+                       bg.coords = buffer_tor_bg.xy, partition = "block")
+tor_block_eval <- tor_block@results
+tor_block_eval <- tor_block_eval[with(tor_block_eval, order(avg.test.or10pct, -avg.test.AUC, delta.AICc)), ]
+View(tor_block_eval)
+# get model
+tor_block_evalMods <- tor_block@models
+names(tor_block_evalMods) <- tor_block@results$settings
+tor_block_Preds <- tor_block@predictions
+tor_block_mod <- tor_block_evalMods[["L_2"]]
+tor_block_proj <- ENMeval::maxnet.predictRaster(tor_block_mod, Env_tor, type = 'cloglog', clamp = TRUE)
+#plot the model prediction
+plot(tor_block_proj)
+points(torquatus_occs[,2:3])  
+
+# Checkerboard 1 ----------------------------------------------------------
+
+
+tor_checkerboard1 <- lapply(X = c(1, 2), build_enm, occs = thinned_tor_occs[,2:3], env = tor_envsBgMsk,
+                            bg.coords = buffer_tor_bg.xy, partition = "checkerboard1")
+
+tor_checkerboard1.1 <- tor_checkerboard1[[1]]
+tor_checkerboard1.2 <- tor_checkerboard1[[2]]
+
+tor_checkerboard1.1_eval <- tor_checkerboard1.1@results
+tor_checkerboard1.1_eval <- tor_checkerboard1.1_eval[with(tor_checkerboard1.1_eval, order(avg.test.or10pct, -avg.test.AUC, delta.AICc)), ]
+View(tor_checkerboard1.1_eval)
+# get model
+tor_checkerboard1.1_evalMods <- tor_checkerboard1.1@models
+names(tor_checkerboard1.1_evalMods) <- tor_checkerboard1.1@results$settings
+tor_checkerboard1.1_Preds <- tor_checkerboard1.1@predictions
+tor_checkerboard1.1_mod <- tor_checkerboard1.1_evalMods[["LQH_4"]]
+tor_checkerboard1.1_proj <- ENMeval::maxnet.predictRaster(tor_checkerboard1.1_mod, Env_tor, type = 'cloglog', clamp = TRUE)
+#plot the model prediction
+plot(tor_checkerboard1.1_proj)
+
+tor_checkerboard1.2_eval <- tor_checkerboard1.2@results
+tor_checkerboard1.2_eval <- tor_checkerboard1.2_eval[with(tor_checkerboard1.2_eval, order(avg.test.or10pct, -avg.test.AUC, delta.AICc)), ]
+View(tor_checkerboard1.2_eval)
+# get model
+tor_checkerboard1.2_evalMods <- tor_checkerboard1.2@models
+names(tor_checkerboard1.2_evalMods) <- tor_checkerboard1.2@results$settings
+tor_checkerboard1.2_Preds <- tor_checkerboard1.2@predictions
+tor_checkerboard1.2_mod <- tor_checkerboard1.2_evalMods[["LQ_3.5"]]
+tor_checkerboard1.2_proj <- ENMeval::maxnet.predictRaster(tor_checkerboard1.2_mod, Env_tor, type = 'cloglog', clamp = TRUE)
+#plot the model prediction
+plot(tor_checkerboard1.2_proj)
+
+tor_checkerboard1.10 <- build_enm(occs = thinned_tor_occs[,2:3], env = tor_envsBgMsk,
+                                  bg = buffer_tor_bg.xy, partition = "checkerboard1", aggregation.factor = 10)
+tor_checkerboard1.10_eval <- tor_checkerboard1.10@results
+tor_checkerboard1.10_eval <- tor_checkerboard1.10_eval[with(tor_checkerboard1.10_eval, order(avg.test.or10pct, -avg.test.AUC, delta.AICc)), ]
+View(tor_checkerboard1.10_eval)
+# get model
+tor_checkerboard1.10_evalMods <- tor_checkerboard1.10@models
+tor_checkerboard1.10_Preds <- tor_checkerboard1.10@predictions
+tor_checkerboard1.10_mod <- tor_checkerboard1.10_evalMods[["LQ5.0"]]
+tor_checkerboard1.10_proj <- ENMeval::maxnet.predictRaster(tor_checkerboard1.10_mod, Env_tor, type = 'cloglog', doClamp = TRUE)
+#plot the model prediction
+plot(tor_checkerboard1.10_proj)
+
+# Checkerboard 2 ----------------------------------------------------------
+
+tor_checkerboard2 <- lapply(X = c(1, 2), build_enm, occs = thinned_tor_occs[,2:3], env = tor_envsBgMsk,
+                            bg.coords = buffer_tor_bg.xy, partition = "checkerboard2")
+
+tor_checkerboard2.1 <- tor_checkerboard2[[1]]
+tor_checkerboard2.2 <- tor_checkerboard2[[2]]
+
+tor_checkerboard2.1_eval <- tor_checkerboard2.1@results
+tor_checkerboard2.1_eval <- tor_checkerboard2.1_eval[with(tor_checkerboard2.1_eval, order(avg.test.or10pct, -avg.test.AUC, delta.AICc)), ]
+View(tor_checkerboard2.1_eval)
+# get model
+tor_checkerboard2.1_evalMods <- tor_checkerboard2.1@models
+names(tor_checkerboard2.1_evalMods) <- tor_checkerboard2.1@results$settings
+tor_checkerboard2.1_Preds <- tor_checkerboard2.1@predictions
+tor_checkerboard2.1_mod <- tor_checkerboard2.1_evalMods[["LQH_4"]]
+tor_checkerboard2.1_proj <- ENMeval::maxnet.predictRaster(tor_checkerboard2.1_mod, Env_tor, type = 'cloglog', clamp = TRUE)
+#plot the model prediction
+plot(tor_checkerboard2.1_proj)
+
+tor_checkerboard2.2_eval <- tor_checkerboard2.2@results
+tor_checkerboard2.2_eval <- tor_checkerboard2.2_eval[with(tor_checkerboard2.2_eval, order(avg.test.or10pct, -avg.test.AUC, delta.AICc)), ]
+View(tor_checkerboard2.2_eval)
+# get model
+tor_checkerboard2.2_evalMods <- tor_checkerboard2.2@models
+names(tor_checkerboard2.2_evalMods) <- tor_checkerboard2.2@results$settings
+tor_checkerboard2.2_Preds <- tor_checkerboard2.2@predictions
+tor_checkerboard2.2_mod <- tor_checkerboard2.2_evalMods[["LQ_0.5"]]
+tor_checkerboard2.2_proj <- ENMeval::maxnet.predictRaster(tor_checkerboard2.2_mod, Env_tor, type = 'cloglog', clamp = TRUE)
+#plot the model prediction
+plot(tor_checkerboard2.2_proj)
